09e368507722f19d4cfdfce05acdf2c1b4cd65db
Aabhash <ad_flint@hotmail.com>
2017-02-07 12:01:54 +0545

    Add Javascript animated background

--- Files/JS/JavascriptFile.js
+++
@@ -1,9 +0,0 @@
-function TabExpand() 
-{
-    var x = document.getElementById("TabListId");
-    if (x.className === "TabList") {
-        x.className += "responsive";
-    } else {
-        x.className = "TabList";
-    }
-}
