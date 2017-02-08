1c49797323daeeab1d98dcfbbb0457e26f22383c
Aabhash <ad_flint@hotmail.com>
2017-02-09 01:07:31 +0545

    Finish Client side scoring system

--- Files/js/questions.js
+++ Files/js/questions.js
@@ -1,10 +1,11 @@
 function clearradio()
 {
-	var all = document.getElementsByTagName("input");
+	var all = document.getElementsByClassName("radio4");
 	for( var i = 0; i<all.length; i++)
 	{
-		all[i].checked = false;
+		all[i].checked = true;
 	}
+	renable();
 }
 
 function validate()
@@ -14,33 +15,18 @@ function validate()
 
 	if(buttonText.innerHTML == "NEXT")
 	{
-		var isValidFirst = checkValidFirst();
-		if(isValidFirst = true)
-		{
-			changeTable(); 
-			pr.style.width ="50%";
-			pr.innerHTML = "1/2";
-			buttonText.innerHTML ="FINISH";
-		}
-		else
-		{
-			alert("Please select an option"); 
-		}
+		changeTable(); 
+		pr.style.width ="50%";
+		pr.innerHTML = "1/2";
+		buttonText.innerHTML ="FINISH";
+		
 	}
 	else if (buttonText.innerHTML == "FINISH")
 	{
-		var isValidSecond = checkValidSecond();
-		if(isValidSecond = true)
-		{
-			load(); 
-			pr.style.width ="100%";
-			pr.innerHTML = "Complete";
-			buttonText.innerHTML ="FINISHED";
-		}
-		else
-		{
-			alert("Please select an option"); 
-		}
+		pr.style.width ="100%";
+		pr.innerHTML = "Complete";
+		buttonText.innerHTML ="FINISHED";
+		load(); 
 	}
 }
 
@@ -54,55 +40,78 @@ function changeTable()
 
 }
 
-function checkValidFirst()
-{
-
-	var c1 = document.getElementsByName("Q1");
-	var c2 = document.getElementsByName("Q2");
-	var c3 = document.getElementsByName("Q3");
-	var c4 = document.getElementsByName("Q4");
-	var c5 = document.getElementsByName("Q5");
-	var x = [c1,c2,c3,c4,c5];
-	var t = valid(x);
-	return t;
-
-}
-
-function checkValidSecond()
-{
-	var c1 = document.getElementsByName("Q6");
-	var c2 = document.getElementsByName("Q7");
-	var c3 = document.getElementsByName("Q8");
-	var c4 = document.getElementsByName("Q9");
-	var c5 = document.getElementsByName("Q10");
-	var x = [c1,c2,c3,c4,c5];
-	var t =	valid(x);
-	return t;
-}
-
-function valid(a)
-{
-	for(var i = 0; i < a.length; i++)
-	{
-		alert("Hello"+ a[i]);
-		var isi = false;
-		for(var j = 0; j < a[i].length; j++)
-		{
-			if(a[i][j].checked = true)
-			{
-				isi = true;	
-				break;
-			}
-			alert("Hello");
-		}		
-		if(isi = false)
-		{
-			return false;
-		}
-	}
-	return false;
-}
 function load()
 {
-	alert("Load"); 
+	var e = 0, s = 0;
+	var arr = [e,s];
+	arr = score(e,s);
+	rdisable();
+	alert("Load");
+}
+
+function score(escore,sscore)
+{
+	var a = document.getElementsByTagName("input");
+
+	for(var j = 0; j < a.length; j++)
+	{
+		var y = a[j];
+		if(y.checked)
+		{
+			if(y.name == "Q1" || y.name == "Q2" || y.name == "Q3" || y.name == "Q4" || y.name == "Q5")
+			{
+				if(y.className == "radio1")
+					sscore += 4;
+				else if(y.className == "radio2")
+					sscore += 2;
+				else if(y.className == "radio3")
+					sscore += 1;
+				else if(y.className == "radio4")
+					sscore += 0;
+				else if(y.className == "radio5")
+					sscore -= 1;
+				else if(y.className == "radio6")
+					sscore -= 2;
+				else if(y.className == "radio7")
+					sscore -= 4;	
+			}
+			else
+			{
+				if(y.className == "radio1")
+					escore += 4;
+				else if(y.className == "radio2")
+					escore += 2;
+				else if(y.className == "radio3")
+					escore += 1;
+				else if(y.className == "radio4")
+					escore += 0;
+				else if(y.className == "radio5")
+					escore -= 1;
+				else if(y.className == "radio6")
+					escore -= 2;
+				else if(y.className == "radio7")
+					escore -= 4;
+			}
+
+		}
+	}
+	return [escore,sscore];
+}
+
+function rdisable()
+{
+	var a = document.getElementsByTagName("input");
+	for(var x = 0; x < a.length; x++)
+	{
+		a[x].disabled = true;
+	}
+}
+
+function renable()
+{
+	var a = document.getElementsByTagName("input");
+	for(var x = 0; x < a.length; x++)
+	{
+		a[x].disabled = false;
+	}
 }
