2846e2a5dab02cfc112e48f7240a5fc734c66861
Aabhash <ad_flint@hotmail.com>
2017-02-08 23:20:19 +0545

    Radio Validation

--- Files/js/questions.js
+++ Files/js/questions.js
@@ -12,29 +12,35 @@ function validate()
 	var buttonText = document.getElementById('buttonspan');
 	var x = document.getElementById('pr');
 
-	var isValidFirst = checkValidFirst();
-	var isValidSecond = checkValidSecond();
-
-	if(buttonText.innerHTML == "NEXT" && isValidFirst)
+	if(buttonText.innerHTML == "NEXT")
 	{
-		changeTable(); 
-
-		pr.style.width ="50%";
-		pr.innerHTML = "50%";
-		buttonText.innerHTML ="FINISH";
+		var isValidFirst = checkValidFirst();
+		if(isValidFirst = true)
+		{
+			changeTable(); 
+			pr.style.width ="50%";
+			pr.innerHTML = "1/2";
+			buttonText.innerHTML ="FINISH";
+		}
+		else
+		{
+			alert("Please select an option"); 
+		}
 	}
-	else if (buttonText.innerHTML == "FINISH" && isValidSecond)
+	else if (buttonText.innerHTML == "FINISH")
 	{
-		load(); 
-
-		pr.style.width ="100%";
-		pr.innerHTML = "100%";
-
-		buttonText.innerHTML ="FINISHED";
-	}
-	else
-	{
-		alert("Please select an option"); 
+		var isValidSecond = checkValidSecond();
+		if(isValidSecond = true)
+		{
+			load(); 
+			pr.style.width ="100%";
+			pr.innerHTML = "Complete";
+			buttonText.innerHTML ="FINISHED";
+		}
+		else
+		{
+			alert("Please select an option"); 
+		}
 	}
 }
 
@@ -50,44 +56,48 @@ function changeTable()
 
 function checkValidFirst()
 {
-	var c1 = document.getElementByName("Q1");
-	var c2 = document.getElementByName("Q2");
-	var c3 = document.getElementByName("Q3");
-	var c4 = document.getElementByName("Q4");
-	var c5 = document.getElementByName("Q5");
+
+	var c1 = document.getElementsByName("Q1");
+	var c2 = document.getElementsByName("Q2");
+	var c3 = document.getElementsByName("Q3");
+	var c4 = document.getElementsByName("Q4");
+	var c5 = document.getElementsByName("Q5");
 	var x = [c1,c2,c3,c4,c5];
-	valid(x);
+	var t = valid(x);
+	return t;
 
 }
 
 function checkValidSecond()
 {
-	var c1 = document.getElementByName("Q6");
-	var c2 = document.getElementByName("Q7");
-	var c3 = document.getElementByName("Q8");
-	var c4 = document.getElementByName("Q9");
-	var c5 = document.getElementByName("Q10");
+	var c1 = document.getElementsByName("Q6");
+	var c2 = document.getElementsByName("Q7");
+	var c3 = document.getElementsByName("Q8");
+	var c4 = document.getElementsByName("Q9");
+	var c5 = document.getElementsByName("Q10");
 	var x = [c1,c2,c3,c4,c5];
-	valid(x);
+	var t =	valid(x);
+	return t;
 }
 
 function valid(a)
 {
-	var is = false;
-	for(var i = 0; i<x.length; i++)
+	for(var i = 0; i < a.length; i++)
 	{
+		alert("Hello"+ a[i]);
 		var isi = false;
-		for(var j = 0; j < x[i].length; j++)
+		for(var j = 0; j < a[i].length; j++)
 		{
-			if(x[i][j].checked == true)
+			if(a[i][j].checked = true)
 			{
 				isi = true;	
 				break;
 			}
+			alert("Hello");
 		}		
-		if( is == true)
+		if(isi = false)
 		{
-			return true;
+			return false;
 		}
 	}
 	return false;
