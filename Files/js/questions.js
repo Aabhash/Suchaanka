dfec44fa73ee15ba349938d85db9d69791c37983
Aabhash <ad_flint@hotmail.com>
2017-02-08 18:31:12 +0545

    Add Form Controls

--- Files/js/questions.js
+++ Files/js/questions.js
@@ -1,6 +1,29 @@
-﻿
-$(document).ready(function()
+function changeTable()
 {
-		$(".QuestionSet1").css("display" : "none");
-	
-});
+	var newTable = document.getElementById('QuestionSet2');
+	var oldTable = document.getElementById('QuestionSet1');
+	var buttonText = document.getElementById('buttonspan');
+	var x = document.getElementById('pr');
+
+	if(buttonText.innerHTML == "NEXT")
+	{
+		newTable.style.display="inline";
+		oldTable.style.display="none";
+		buttonText.innerHTML ="FINISH";
+
+		pr.style.width ="50%";
+		pr.innerHTML = "50%";
+	}
+	else
+	{
+		if(buttonText.innerHTML == "FINISH")
+		{
+			pr.style.width ="100%";
+			pr.innerHTML = "100%";
+
+			alert("Load");
+			buttonText.innerHTML ="FINISHED";
+		}
+	}
+
+}
