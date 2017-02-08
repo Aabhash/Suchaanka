d715a68687e7a2b9b31ad8f3776dd703766df8e9
Aabhash <ad_flint@hotmail.com>
2017-02-08 21:22:42 +0545

    Validate Question.html radio buttons

--- Files/js/questions.js
+++ Files/js/questions.js
@@ -9,35 +9,44 @@ function clearradio()
 
 function validate()
 {
+	var buttonText = document.getElementById('buttonspan');
+	var x = document.getElementById('pr');
+
+	var isValid = checkValid();
+
+	if(buttonText.innerHTML == "NEXT" && isValidFirst)
+	{
+		changeTable(); 
+
+		pr.style.width ="50%";
+		pr.innerHTML = "50%";
 	
+	}
+	else if (buttonText.innerHTML == "FINISH" && isValidSecond)
+	{
+		alert("load");	
+		pr.style.width ="100%";
+		pr.innerHTML = "100%";
+
+		buttonText.innerHTML ="FINISHED";
+	}
+	else
+	{
+		alert("Please select an option"); 
+	}
 }
+
 function changeTable()
 {
 	var newTable = document.getElementById('QuestionSet2');
 	var oldTable = document.getElementById('QuestionSet1');
-	var buttonText = document.getElementById('buttonspan');
-	var x = document.getElementById('pr');
+	
+	newTable.style.display="inline";		
+	oldTable.style.display="none";
+	buttonText.innerHTML ="FINISH";
+}
 
-	if(buttonText.innerHTML == "NEXT")
-	{
-		newTable.style.display="inline";
-		oldTable.style.display="none";
-		buttonText.innerHTML ="FINISH";
-
-		pr.style.width ="50%";
-		pr.innerHTML = "50%";
-	}
-	else
-	{
-		if(buttonText.innerHTML == "FINISH")
-		{
-			pr.style.width ="100%";
-			pr.innerHTML = "100%";
-
-			alert("Load");
-			buttonText.innerHTML ="FINISHED";
-		}
-	}
-
+checkValid()
+{
 }
 
