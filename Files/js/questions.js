041152d01ddcd612e1c12f2b43e6c6b1b04dcb32
Aabhash <ad_flint@hotmail.com>
2017-02-08 21:47:08 +0545

    Add IsValid() function

--- Files/js/questions.js
+++ Files/js/questions.js
@@ -12,7 +12,8 @@ function validate()
 	var buttonText = document.getElementById('buttonspan');
 	var x = document.getElementById('pr');
 
-	var isValid = checkValid();
+	var isValidFirst = checkValidFirst();
+	var isValidSecond = checkValidSecond();
 
 	if(buttonText.innerHTML == "NEXT" && isValidFirst)
 	{
@@ -20,11 +21,12 @@ function validate()
 
 		pr.style.width ="50%";
 		pr.innerHTML = "50%";
-	
+		buttonText.innerHTML ="FINISH";
 	}
 	else if (buttonText.innerHTML == "FINISH" && isValidSecond)
 	{
-		alert("load");	
+		load(); 
+
 		pr.style.width ="100%";
 		pr.innerHTML = "100%";
 
@@ -43,10 +45,54 @@ function changeTable()
 	
 	newTable.style.display="inline";		
 	oldTable.style.display="none";
-	buttonText.innerHTML ="FINISH";
+
 }
 
-checkValid()
+function checkValidFirst()
 {
+	var c1 = document.getElementByName("Q1");
+	var c2 = document.getElementByName("Q2");
+	var c3 = document.getElementByName("Q3");
+	var c4 = document.getElementByName("Q4");
+	var c5 = document.getElementByName("Q5");
+	var x = [c1,c2,c3,c4,c5];
+	valid(x);
+
 }
 
+function checkValidSecond()
+{
+	var c1 = document.getElementByName("Q6");
+	var c2 = document.getElementByName("Q7");
+	var c3 = document.getElementByName("Q8");
+	var c4 = document.getElementByName("Q9");
+	var c5 = document.getElementByName("Q10");
+	var x = [c1,c2,c3,c4,c5];
+	valid(x);
+}
+
+function valid(a)
+{
+	var is = false;
+	for(var i = 0; i<x.length; i++)
+	{
+		var isi = false;
+		for(var j = 0; j < x[i].length; j++)
+		{
+			if(x[i][j].checked == true)
+			{
+				isi = true;	
+				break;
+			}
+		}		
+		if( is == true)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+function load()
+{
+	alert("Load"); 
+}
