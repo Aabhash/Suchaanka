9547fbec82e966a11125c8ad031a667edc644f68
Aabhash <ad_flint@hotmail.com>
2017-02-10 00:27:12 +0545

    Fix Results Logic

--- Files/js/questions.js
+++ Files/js/questions.js
@@ -1,3 +1,4 @@
+//Clear all radio buttons
 function clearradio()
 {
 	var all = document.getElementsByClassName("radio4");
@@ -30,6 +31,7 @@ function validate()
 	}
 }
 
+//Change table from question set 1 to question set 2
 function changeTable()
 {
 	var newTable = document.getElementById('QuestionSet2');
@@ -40,18 +42,19 @@ function changeTable()
 
 }
 
+//Server Side Load
 function load()
 {
 	var e = 0, s = 0;
 	var arr = [e,s];
 	arr = score(e, s);
 	rdisable();
-	var a = (arr[0] * 2.4 / 10);
-    var b = (arr[1] * 2.4 / 10)
-	window.open("http://localhost:57390/Results.aspx?score="+a+b, "_self");
+	var a = Math.floor(arr[0] * 1.5 / 10);
+	var b = Math.floor(arr[1] * 1.5 / 10);
+	window.open("http://localhost:57390/Results.aspx?score=" + a + b, "_self");
 }
 
-
+//Calculate Score
 function score(escore,sscore)
 {
 	var a = document.getElementsByTagName("input");
@@ -64,36 +67,36 @@ function score(escore,sscore)
 			if(y.name == "Q1" || y.name == "Q2" || y.name == "Q3" || y.name == "Q4" || y.name == "Q5")
 			{
 				if(y.className == "radio1")
-					sscore += 4;
+					sscore += 6;
 				else if(y.className == "radio2")
-					sscore += 2;
+					sscore += 4;
 				else if(y.className == "radio3")
-					sscore += 1;
+					sscore += 2;
 				else if(y.className == "radio4")
 					sscore += 0;
 				else if(y.className == "radio5")
-					sscore -= 1;
-				else if(y.className == "radio6")
 					sscore -= 2;
+				else if(y.className == "radio6")
+					sscore -= 4;
 				else if(y.className == "radio7")
-					sscore -= 4;	
+					sscore -= 6;	
 			}
 			else
 			{
 				if(y.className == "radio1")
-					escore += 4;
+					escore += 6;
 				else if(y.className == "radio2")
-					escore += 2;
+					escore += 4;
 				else if(y.className == "radio3")
-					escore += 1;
+					escore += 2;
 				else if(y.className == "radio4")
 					escore += 0;
 				else if(y.className == "radio5")
-					escore -= 1;
-				else if(y.className == "radio6")
 					escore -= 2;
-				else if(y.className == "radio7")
+				else if(y.className == "radio6")
 					escore -= 4;
+				else if(y.className == "radio7")
+					escore -= 6;
 			}
 
 		}
@@ -101,6 +104,7 @@ function score(escore,sscore)
 	return [escore,sscore];
 }
 
+//Disable every Radio Button once done
 function rdisable()
 {
 	var a = document.getElementsByTagName("input");
@@ -110,6 +114,7 @@ function rdisable()
 	}
 }
 
+//Disable every Radio Button at start
 function renable()
 {
 	var a = document.getElementsByTagName("input");
