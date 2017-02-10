86cfb99d588ae93620473477ede160fb3bf4e56e
Shreesha <shreeshapokharel@gmail.com>
2017-02-10 18:59:59 +0545

    Change validation

--- Files/js/Contact.js
+++ Files/js/Contact.js
@@ -1,50 +1,45 @@
-function validate(){
+function validate(){				
 				
+				 	var re = /\S+@\S+\.\S+/;
+					var firstname = document.getElementById('fname').value;
+					var lastname = document.getElementById('lname').value;
+					var sub = document.getElementById('subject').value;
+					var em = document.getElementById('email').value;
+                        submit = true;
+                if(firstname == '')
+                        {
+                            document.getElementById('fname_error').innerHTML = "First Name Is Required";
+                            return false;
+                        }                     
+                
 				
-				
-				var	firstname= document.f1.fname.value;
-				var lastname=document.f1.lname.value;
-				var email=document.f1.email.value;
-				var sub=document.f1.subject.value;
-				var dotpos = email.lastIndexOf('.');
-				var firstdot = email.indexOf('.');
-				var atpos = email.indexOf('@');
-				if(firstname == '' ){
-				    alert("First Name should not be empty");
-					return false;
+				if(lastname ==''){
+				   document.getElementById('lname_error').innerHTML = "Last Name Is Required";
+                            return false;
 				}
-				else if(lastname ==''){
-				    var x = document.getElementById('ln');
-				    x.innerText = ("Error");
-				    x.style.backgroundColor = ("red");
-					return false;
-				}
-				else if(sub ==''){
-					alert("Subject should not be empty");
-					return false;
+				if(sub ==''){
+					document.getElementById('sub_error').innerHTML = "Subject Is Required";
+                            return false;
 				}
 				
-				else if(email ==''){
-					alert('Email should not be empty');
-					return false;
+				if(em ==''){
+					 document.getElementById('em_error').innerHTML = "Email Is Required";
+                            return false;
+
 				}
-				else if(atpos < 0 || dotpos<0){
-					alert('Email is not valid! Email should contain @ and . sign');
-					return false;
-				}
-				else if(dotpos==0 || atpos ==0 || firstdot==0){
-					alert('Email is not valid! Email should not contain @ or dot at begining');
-					return false;
-				}
-				else if(dotpos - atpos <3){
-					alert('Email is not valid! There should be atleast two characters between @ and dot sign');
-					return false;
-				}
-				else if(email.length - dotpos < 3){
-					alert('Email is not valid! Atleast two characters required after last dot');
-					return false;
-				}				
 				
+				if (em!='')
+				{
+					 if (!( re.test(em))){
+				   
+				       	 document.getElementById('em_error').innerHTML = "Email Is INVALID!";
+                          return false;
+				    }
+				}
 				
-				return true;
-			}
+					return submit;
+				
+				   
+ 
+ 			}
+
