fb6976c51800c64e15b6f754d4888909b9a192dc
Aabhash <ad_flint@hotmail.com>
2017-02-12 19:58:35 +0545

    Add remarks

--- js/Results.js
+++ js/Results.js
@@ -26,5 +26,26 @@
 	//Set Image property
 	ImageDiv.style.marginLeft = mi + 'px';
 	ImageDiv.style.marginTop = ni+ 'px';
+
+	if(m > 0 && n > 0)
+	{
+		Remarks.innerHTML += "<h1><br>Authoritarian Right Wing</h1><hr> Examples <br> Margaret Thatcher Theresa May Donald Trump Narendra Modi "
+	}
+	else if(m >0 && n < 0)
+	{
+		Remarks.innerHTML += "<h1><br>Authoritarian Left Wing </h1><hr> Examples <br> Xi-Jinping KP Oli"
+	}
+	else if(m < 0 && n < 0)
+	{
+		Remarks.innerHTML += "<h1><br>Libertarian Left Wing </h1><hr> Examples <br> Bernie Sanders Jeremy Corbyn Benoit Hamon"
+	}
+	else if(m < 0 && n > 0)
+	{
+		Remarks.innerHTML +="<h1><br>Libertarian Right Wing </h1><hr> Examples <br> David Cameron Rand Paul "
+	}
+	else
+	{
+		Remarks.innerHTML +="<h1><br>Centrist </h1><hr> Examples <br><br> Barack Obama, Justin Trudeau, Hilary Clinton, Tony Blair"
+	}
 }
 
