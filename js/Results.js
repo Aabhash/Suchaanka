404b6a5245abc1189e16df23df61ae2799ff4704
Aabhash <ad_flint@hotmail.com>
2017-02-12 17:45:43 +0545

    Convert ASP mapping code to JS mapping code

--- js/Results.js
+++ js/Results.js
@@ -1,30 +1,32 @@
-﻿function displayResults() {
-	alert("Hello");
-	var x = Request.querystring["score"];
+﻿function computeRes() {
+
+	var x = window.location.search.slice(7);
+
 	var m = 0, n = 0;
-	if (x.Length == 2) {
-		m = parseInt(x[0]);
-		n = parseInt(x[1]);
+	if (x.length == 2) {
+		m = x[0];
+		n = x[1];
 	}
-	else if (x.Length == 3) {
+	else if (x.length == 3) {
 		if (x[0] == '-') {
-			m = 0 - parseInt(x[1]);
-			n = parseInt(x[2]);
+			m = 0 - x[1];
+			n = x[2];
 		}
 		else {
-			m = parseInt(x[0]);
-			n = 0 - parseInt(x[2]);
+			m = x[0];
+			n = 0 - x[2];
 		}
 	}
-	else if (x.Length == 4) {
-		m = 0 - parseInt(x[1]);
-		n = 0 - parseInt(x[3]);
+	else if (x.length == 4) {
+		m = 0 - x[1];
+		n = 0 - x[3];
 	}
 	//Max value = (1.5 * 6) * 16
 	var mi = m * 16;
 	var ni = n * 16;
 
-	ImageDiv.Style.margin - left = mi;
-	ImageDiv.Style.margin - top = ni;
+	//Set Image property
+	ImageDiv.style.marginLeft = mi + 'px';
+	ImageDiv.style.marginTop = ni+ 'px';
 }
 
