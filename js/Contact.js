ba2bb0a7ca18c8c1a477d91462d652ebfca86a86
Aabhash <ad_flint@hotmail.com>
2017-02-13 18:11:22 +0545

    Format Results.js

--- js/Contact.js
+++ js/Contact.js
@@ -1,58 +1,55 @@
 
-function validate(){				
-				
-				 	var re = /\S+@\S+\.\S+/;
-					var firstname = document.getElementById('fname').value;
-					var lastname = document.getElementById('lname').value;
-					var sub = document.getElementById('subject').value;
-					var em = document.getElementById('email').value;
-					submit = true;
+function validate() {
+
+	var re = /\S+@\S+\.\S+/;
+	var firstname = document.getElementById('fname').value;
+	var lastname = document.getElementById('lname').value;
+	var sub = document.getElementById('subject').value;
+	var em = document.getElementById('email').value;
+	submit = true;
 
 
-                if(firstname == '')
-                        {
-                            document.getElementById('fname_error').innerHTML = "First Name Is Required";
-                            fname.focus();
-                            return false;
-                        }                     
-                
-				
-				
-				
-				if(lastname ==''){
-				   document.getElementById('lname_error').innerHTML = "Last Name Is Required";
-				   lname.focus();
-                   return false;
-				}
-				if(sub ==''){
-					document.getElementById('sub_error').innerHTML = "Subject Is Required";
-                    return false;
-				}
-				
-				if(em ==''){
-					 document.getElementById('em_error').innerHTML = "Email Is Required";
-                     return false;
+	if (firstname == '') {
+		document.getElementById('fname_error').innerHTML = "First Name Is Required";
+		fname.focus();
+		return false;
+	}
 
-				}
-				
-				if (em!='')
-				{
-					 if (!( re.test(em))){
-				   
-				       	 document.getElementById('em_error').innerHTML = "Email Is INVALID!";
-                         return false;
-				    }
-				}
-				return submit;
-			}
 
-	function removewarning(e)
-    {
-    	var y = e.target;
-    	var m = y.id+'_error';
-    	var n = document.getElementById(m);
-        n.innerHTML = "";
-    }
- 
- 		
+
+
+	if (lastname == '') {
+		document.getElementById('lname_error').innerHTML = "Last Name Is Required";
+		lname.focus();
+		return false;
+	}
+	if (sub == '') {
+		document.getElementById('sub_error').innerHTML = "Subject Is Required";
+		return false;
+	}
+
+	if (em == '') {
+		document.getElementById('em_error').innerHTML = "Email Is Required";
+		return false;
+
+	}
+
+	if (em != '') {
+		if (!(re.test(em))) {
+
+			document.getElementById('em_error').innerHTML = "Email Is INVALID!";
+			return false;
+		}
+	}
+	return submit;
+}
+
+function removewarning(e) {
+	var y = e.target;
+	var m = y.id + '_error';
+	var n = document.getElementById(m);
+	n.innerHTML = "";
+}
+
+
 
