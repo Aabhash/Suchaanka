e51a622a1fb3ae4a082c06efa6ab444dfaf1eca5
Shreesha <shreeshapokharel@gmail.com>
2017-02-14 15:38:46 +0545

    Check spaces as input in Contact

--- js/Contact.js
+++ js/Contact.js
@@ -4,47 +4,57 @@ function validate() {
 	var firstname = document.getElementById('fname').value;
 	var lastname = document.getElementById('lname').value;
 	var sub = document.getElementById('subject').value;
-
 	var em = document.getElementById('e-mail').value;
-
 	var comment=document.getElementById('comment').value;
 
-	submit = true;
+	var submit=false;
+	//Trim first and last spaces from all values
+	firstname=firstname.trim();
+	lastname=lastname.trim();
+	sub=sub.trim();
+	em=em.trim();
+	comment=comment.trim();
 
 	if (firstname == '') {
 		document.getElementById('fname_error').innerHTML = "First Name Is Required";
 		fname.blur();
-		return false;
+		//return false;
 	}
 
 	if (lastname == '') {
 		document.getElementById('lname_error').innerHTML = "Last Name Is Required";
 		lname.blur();
-		return false;
+		//return false;
 	}
 	if (sub == '') {
 		document.getElementById('subject_error').innerHTML = "Subject Is Required";
 		subject.blur();
-		return false;
+		//return false;
 	}
 	if (em == '') {
 		document.getElementById('e-mail_error').innerHTML = "Email Is Required";
-		e - mail.blur();
-		return false;
+		e-mail.blur();
+		//return false;
 	}
 	if(comment=='')
 	{
 		document.getElementById('comment_error').innerHTML = "Comment Is Required";
-		return false;
+		comment.blur();
+		//return false;
 	}
 
 	if (em != '') {
 		if (!(re.test(em))) {
 
 			document.getElementById('e-mail_error').innerHTML = "Email Is INVALID!";
-			return false;
+			e-mail.blur();
+			//return false;
 		}
 	}
+	else{
+		submit=true;
+	}
+
 	return submit;
 }
 
@@ -58,13 +68,15 @@ function removewarning(e) {
 
 
 function reset(){
-
-
 	document.getElementById('fname').value = "";
 	document.getElementById('lname').value = "";
 	document.getElementById('subject').value = "";
 	document.getElementById('e-mail').value= "";
 	document.getElementById('comment').value = "";
+}
 
-
+function sendMail()
+{
+	var res=validate();
+    alert(res); 
 }
